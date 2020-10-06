5da80fedef626a36ee4951c081be947cf5caab5c
Andrew Sova <andrii.sova@tbwa-x.com>
2020-10-06 07:43:34 +0300

    Add ejs engine

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # book-shop
+
+## Getting Started
+To test, contribute or just see what we did follow few easy steps:
+- clone the repository
+- cd to the directory with the repository
+- run `yarn install` (or `npm install` if you don't use yarn)
+- run the app using `yarn start` (or `npm start`)
+- enjoy!
--- app.js
+++ app.js
@@ -7,15 +7,7 @@ const expressHbs = require('express-handlebars');
 const PORT = 3000;
 const app = express();
 
-app.engine(
-  'hbs',
-  expressHbs({
-    layoutsDir: 'views/layouts/',
-    defaultLayout: 'main-layout',
-    extname: 'hbs'
-  })
-);
-app.set('view engine', 'hbs');
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminData = require('./routes/admin');
@@ -28,7 +20,12 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).render('not-found', {pageTitle: 'Page Not Found'});
+  res.status(404).render(
+    'not-found',
+    {
+      pageTitle: 'Page Not Found',
+      path: '/not-found'
+    });
 });
 
 app.listen(PORT);
